b8de1e322ea49588ed93dd328375e567a60936aa
lakidu258 <127181400+lakidu258@users.noreply.github.com>
2023-10-07 09:02:16 +0530

    Edited by Lakidu

--- src/main/java/com/example/sacms/ActivityManagementApplication.java
+++ src/main/java/com/example/sacms/ActivityManagementApplication.java
@@ -14,7 +14,7 @@ public class ActivityManagementApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ActivityManagementApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Ronaldo better than Messi!");
+        stage.setTitle("Ronaldo always better than Messi!");
         stage.setScene(scene);
         stage.show();
     }
