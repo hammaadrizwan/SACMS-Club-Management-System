fbc9c5d1411f4948bd83197aec1ad15b6bf93aae
Hammaad Rizwan <65250595+hammaadrizwan@users.noreply.github.com>
2023-10-22 15:05:45 +0530

    -All Screens are completed -All Methods have been written for important buttons -Have to add labels for incorrect inputs in screens

--- src/main/java/com/example/sacms/Controller.java
+++ src/main/java/com/example/sacms/Controller.java
@@ -8,25 +8,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Controller {
 
-    private Stage stage;
-    private Scene scene;
-
+    private Stage stage;//main stage where all our windows appear
+    private Scene scene;//changes depending on the users requirement each scene is a window
     @FXML
-    public void onSplashScreenButtonClicked(ActionEvent event) throws IOException {
-        Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("loginScreen.fxml"));
+    private AnchorPane userIconButtonOptionPane; //This displays the options available to a user when icon is clicked
+
+    //SCREEN NAVIGATION METHODS
+    @FXML
+    public void onSplashScreenButtonClicked(ActionEvent event) throws IOException {//User is required to login inorder to access the app
+        Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("loginScreen.fxml"));//loads the fxml to the main screen
         this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         this.scene = new Scene(root);
-        this.stage.setTitle("Login");
-        this.stage.setScene(this.scene);
-        this.stage.show();
-        this.stage.setResizable(false);
+        this.stage.setTitle("Login");//Window title
+        this.stage.setScene(this.scene);//sets the scene to the stage
+        this.stage.show();//displays the window
+        this.stage.setResizable(false);//make the window not resizable
     }
+
     @FXML
     public void onDashboardScreenButtonClicked(ActionEvent event) throws IOException {
         Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("dashboard.fxml"));
@@ -36,6 +41,124 @@ public class Controller {
         this.stage.setScene(this.scene);
         this.stage.show();
         this.stage.setResizable(false);
-        System.out.println("WORKS");
     }
+
+    @FXML
+    public void onReportsScreenButtonClicked(ActionEvent event) throws IOException {
+        Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("reportsView.fxml"));
+        this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.scene = new Scene(root);
+        this.stage.setTitle("Reports");
+        this.stage.setScene(this.scene);
+        this.stage.show();
+        this.stage.setResizable(false);
+    }
+    @FXML
+    public void onClubsViewScreenButtonClicked(ActionEvent event) throws IOException {
+        Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("clubsView.fxml"));
+        this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.scene = new Scene(root);
+        this.stage.setTitle("Clubs");
+        this.stage.setScene(this.scene);
+        this.stage.show();
+        this.stage.setResizable(false);
+    }
+    @FXML
+    public void onEventsViewScreenButtonClicked(ActionEvent event) throws IOException {
+        Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("eventsView.fxml"));
+        this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.scene = new Scene(root);
+        this.stage.setTitle("Events");
+        this.stage.setScene(this.scene);
+        this.stage.show();
+        this.stage.setResizable(false);
+    }
+    @FXML
+    public void onSignInButtonClicked(ActionEvent event) throws IOException {
+        Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("signInZero.fxml"));
+        this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.scene = new Scene(root);
+        this.stage.setTitle("Sign in");
+        this.stage.setScene(this.scene);
+        this.stage.show();
+        this.stage.setResizable(false);
+    }
+
+    @FXML
+    public void onScheduleEventButtonOneClicked(ActionEvent event) throws IOException {
+        Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("eventCreation.fxml"));
+        this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.scene = new Scene(root);
+        this.stage.setTitle("Schedule Event");
+        this.stage.setScene(this.scene);
+        this.stage.show();
+        this.stage.setResizable(false);
+    }
+    @FXML
+    public void onCreateClubButtonOneClicked(ActionEvent event) throws IOException {
+        Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("clubCreation.fxml"));
+        this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.scene = new Scene(root);
+        this.stage.setTitle("Create Club");
+        this.stage.setScene(this.scene);
+        this.stage.show();
+        this.stage.setResizable(false);
+    }
+
+    @FXML
+    public void onStudentSignInButtonClicked(ActionEvent event) throws IOException {
+        Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("signInOne.fxml"));
+        this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.scene = new Scene(root);
+        this.stage.setTitle("Student Sign-In");
+        this.stage.setScene(this.scene);
+        this.stage.show();
+        this.stage.setResizable(false);
+    }
+
+    @FXML
+    public void onClubAdvisorSignInButtonClicked(ActionEvent event) throws IOException {
+        Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("signInTwo.fxml"));
+        this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.scene = new Scene(root);
+        this.stage.setTitle("Club Advisor Sign-In");
+        this.stage.setScene(this.scene);
+        this.stage.show();
+        this.stage.setResizable(false);
+    }
+
+    //User options methods when the icon is clicked
+    @FXML
+    public void onExitButtonClicked(ActionEvent event) throws IOException {
+        System.exit(0);// Close the app and terminate the session
+    }
+    public void onUserIconButtonClicked (ActionEvent event) throws IOException {
+        userIconButtonOptionPane.setOpacity(1.00);//Displays the options available
+    }
+    public void onUserIconHideButtonClicked (ActionEvent event) throws IOException {
+        userIconButtonOptionPane.setOpacity(0.00);//hides the buttons
+    }
+    @FXML
+    public void onLogOutButtonClicked(ActionEvent event) throws IOException {
+        Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("loginScreen.fxml"));
+        this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.scene = new Scene(root);
+        this.stage.setTitle("Login");
+        this.stage.setScene(this.scene);
+        this.stage.show();
+        this.stage.setResizable(false);
+    }
+
+
+    public void onRegisterStudentButtonClicked (ActionEvent event) throws IOException {
+        //checks if the fields are valid and then registeres the student to the system
+    }
+    public void onRegisterClubAdvisorButtonClicked (ActionEvent event) throws IOException {
+        //checks if the fields are valid and then registeres the Club advisor to the system
+    }
+
+
+
+
+
 }
