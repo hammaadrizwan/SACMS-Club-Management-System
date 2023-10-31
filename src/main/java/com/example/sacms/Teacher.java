8e36c4ab97d9ff9dfe7c6d5210e1c55fea34691b
Minyyyyyyyy <minudakaliyanage@gmail.com>
2023-10-31 17:46:10 +0530

    Minu teacher class added

---
+++ src/main/java/com/example/sacms/Teacher.java
@@ -0,0 +1,16 @@
+package com.example.sacms;
+
+public class Teacher {
+    private String staffID;
+
+    public String getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(String staffID) {
+        this.staffID = staffID;
+    }
+    public boolean approveClubAdvisor(String clubAdvisorID){
+        return false;
+    }
+}
