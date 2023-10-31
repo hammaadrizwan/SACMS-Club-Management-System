9ce8acf365418d5fd254ae973772aab13c7cd725
Minyyyyyyyy <minudakaliyanage@gmail.com>
2023-10-31 17:41:04 +0530

    Minu person class added

---
+++ src/main/java/com/example/sacms/Person.java
@@ -0,0 +1,62 @@
+package com.example.sacms;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String dateOfBirth;
+    private int contactNo;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public int getContactNo() {
+        return contactNo;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setContactNo(int contactNo) {
+        this.contactNo = contactNo;
+    }
+    public void greetUser(){
+
+    }
+
+}
